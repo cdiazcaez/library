0023afe6c7b99bb145192bbd197e7fab41dc0631
Christian <chris_1693@yahoo.com>
2019-04-21 15:20:39 -0400

    update books.js
    -Set up our HTTP request.
    
    -XMLHttpRequest object can be used to request data from a web server.
    
    -previous commit talk about URLSearchParams
    
    -toString() method returns a string representing the object.
    
    -xhr.open  is the XMLHttpRequest method open() that initializes a newly-created request, or re-initializes an existing one.
    
    -in xhr.onload - // Setup our listener to process completed requests.
    
    xhr.status - Process our return data.
    
    - reeturn callback when the request is successful.

--- public/books.js
+++ public/books.js
@@ -2,3 +2,28 @@ var uri = window.location.search.substring(1);
 var params = new URLSearchParams(uri);
 var format = params.get('format') || 'xml';
 function fetchCategories(callback) {
+
+//  Set up our HTTP request	
+    var xhr = new XMLHttpRequest();
+    var query = new URLSearchParams({
+        format: format,
+        type: 'categories',
+    }).toString();
+
+    xhr.open('GET', 'booklist.php?' + query)
+// Setup our listener to process completed requests
+    xhr.onload = function () {
+        // Process our return data
+        if (xhr.status === 200) {
+            // Runs when the request is successful
+            return callback(
+                    parse(xhr, format, 'category')
+                    );
+        }
+        // Runs when it's not
+        console.log('Request failed: ' + xhr.status);
+    };
+
+    xhr.send();
+
+}

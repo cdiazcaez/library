93343f42af4a73b0af4ffc1170c79b521ce08442
Christian <chris_1693@yahoo.com>
2019-04-21 14:20:51 -0400

    Books.php
    Creating index structure
    adding css file and books.js
    id = categories to fetch the books categories via Ajax.

---
+++ books.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Books</title>
+	<link rel="stylesheet" href="public/books.css"/>
+</head>
+<body>
+
+	<div class="filters">
+		<div id="categories"></div>
+		<button id="filter">List Books</button>
+	</div>
+	
+
+	<div id="books"></div>
+
+
+	<script src="public/books.js"></script>
+</body>
+</html>

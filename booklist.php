87572b586e077a567234fe86c410cf0a4ed7d364
Christian <chris_1693@yahoo.com>
2019-04-22 19:18:57 -0400

    array to xml
    - &$xml_data - passing by reference, function can modify the variable
    
    -foreach construct used to iterate over an array. An array called data, on each iteration the value of current element $field assined to $value
    
    - php operator === for comparison two variables equals and same type

--- booklist.php
+++ booklist.php
@@ -5,3 +5,6 @@ require(__DIR__ . '/classes/book.php');
 $book = new Book();
 
 function array_to_xml( $data, $key, &$xml_data ) {
+    foreach( $data as $field => $value ) {
+    	$tag = $key === null ? $field : $key;
+        if( is_array($value) ) {

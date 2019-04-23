d0b3ac7ef57e4612b7a998cc37e879aefe6abb66
Christian <chris_1693@yahoo.com>
2019-04-22 20:15:43 -0400

    Join SQL tables

--- classes/book.php
+++ classes/book.php
@@ -20,3 +20,20 @@ class Book {
 
 		return $categories;
 	}
+
+	public function fetchByCategory($category) {
+		$stmt = $this->conn->prepare("
+			SELECT b.id, b.title, ba.author, bc.category, y.year, bp.price FROM book b
+			INNER JOIN book_author ba ON ba.book = b.id
+			INNER JOIN book_category bc ON bc.book = b.id
+			INNER JOIN book_year y ON y.book = b.id
+			INNER JOIN book_price bp ON bp.book = b.id
+			WHERE bc.category = ?
+		");
+
+		$stmt->execute([$category]);
+
+		$books = $stmt->fetchAll();
+
+		return $books;
+	}
